4aa5fe810fac36b89773176446342ac56b18958c
Nisarg Hatwar <134367605+NisargHatwar@users.noreply.github.com>
2025-04-01 22:45:09 +0530

    Add files via upload

---
+++ index.mjs
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/api", (req, res) => {
+  console.log(req.body);
+  res.send("Received data successfully!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
